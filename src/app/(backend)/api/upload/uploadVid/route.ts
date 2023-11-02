b52fb1475463edb003b3808f11261c0406cf7655
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-11-02 21:10:39 +0530

    Added: api routes for uploading videos

---
+++ src/app/(backend)/api/upload/uploadVid/route.ts
@@ -0,0 +1,84 @@
+
+import { NextRequest, NextResponse } from "next/server";
+import { pool } from "@/app/(backend)/config/db";
+
+export async function POST(req:NextRequest)
+{
+      try {
+          const data = await req.formData();
+
+          console.log('data: ',data);
+
+        const file:File | null = await data.get('file') as unknown as File;
+        
+        console.log("file: ",file);
+        if(!file)
+        {
+            return NextResponse.json({
+                message:'Cannot get the file',
+                success:false,
+            })
+        }
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+
+        //converting into base64String
+
+        const base64String = buffer.toString('base64');
+
+        ////
+          const uploadToDb = async ()=>
+        {
+               let time = new Date();
+
+        const timeObj = {
+            'title':time.toString(),
+            'hour':time.getHours(),
+            'date':time.getDate(),
+            'day': time.getDay(),
+            'month':time.getMonth()+1,
+            'year':time.getFullYear(),
+            'minutes':time.getMinutes()
+        }
+                try
+                {
+                    await pool.connect();
+ 
+                  const query= await pool.query('INSERT INTO  videos_grievance (title , month, date, year,day ,hour, minutes,vid_data) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)',[timeObj.title,timeObj.month,timeObj.date,timeObj.year,timeObj.day,timeObj.hour,timeObj.minutes,base64String]);
+
+                         console.log("got this response: ",query);
+
+          return NextResponse.json({
+            message:'got the the query',
+            success:true,
+            details:await query,
+          },{
+            status:200
+          });
+        
+
+
+      } catch (e) {
+            console.log("error while uploading to db: ",e);
+        return Response.json({
+            message:e.message,
+        },{
+            status:502
+        })
+    }
+}
+
+return uploadToDb()
+    
+        /////
+}catch(e)
+{
+    console.log("got the error while uploading: ",e.message)
+    return NextResponse.json({
+        message:e.message,
+        success:false,
+    })
+}
+      
+}
+    
