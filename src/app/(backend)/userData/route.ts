8552fd5367a5fa107e4297f8851f7b79f527fd59
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-11-02 21:12:00 +0530

    Added: api route for fetching user data when required or invoked

---
+++ src/app/(backend)/userData/route.ts
@@ -0,0 +1,21 @@
+import {cookies} from 'next/headers'
+import {verify} from 'jsonwebtoken'
+import Decrypt from '@/utils/Decrypt';
+import { NextRequest, NextResponse } from 'next/server';
+export async function GET(req:NextRequest)
+{
+   try {
+     const cookieStore = cookies();
+
+    const encryptedToken = cookieStore.get('Auth')?.value;
+
+    const token:string = Decrypt(encryptedToken);
+
+    const user = await verify(token,process.env.NEXT_PUBLIC_PRIVATE_KEY);
+
+   return NextResponse.json({user:user})
+   } catch (e) {
+    console.log("Error: ",e);
+    return NextResponse.json({user:''})
+   }
+}
