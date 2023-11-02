87e49f11918b3399a874607005c10bb7964056dc
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-11-02 21:07:22 +0530

    Added: middleware inorder to authenticate the user

---
+++ src/middleware.ts
@@ -0,0 +1,49 @@
+
+import { NextResponse, NextRequest } from 'next/server'
+
+import {verify} from 'jsonwebtoken'
+import Decrypt from './utils/Decrypt';
+export async function middleware(req:NextRequest)
+{
+let url = req.nextUrl.clone();
+
+    try {
+  let resp;
+
+     if(req.cookies.has("Auth")){
+       let  encryptedToken =await req.cookies.get("Auth")?.value;
+      
+    const token:string = Decrypt(encryptedToken);
+
+        //redirect to intended location
+         const user = await verify(token,process.env.NEXT_PUBLIC_PRIVATE_KEY);
+
+          resp = NextResponse.next();
+        resp.headers.set('verified','true'); 
+        resp.headers.set('user',await user); 
+  
+
+            url.pathname = '/';
+ return NextResponse.rewrite(url);
+
+}
+else{
+  let resp = NextResponse.next();
+  url.pathname = '/';
+    resp.headers.set("verified",'false');
+    
+ return NextResponse.rewrite(url);
+}
+  
+} catch (e) {
+let resp = NextResponse.next();
+  resp.headers.set("verified",'false');
+ return NextResponse.rewrite(url);
+
+}
+  
+}
+
+export const config = {
+    matcher: ['/random' ],
+};
