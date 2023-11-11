8ed81f6d470260c90aa9a1ff36f949e468321313
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-11-11 07:44:55 +0530

    Aaded the grievance route

---
+++ src/app/(frontend)/Grievieance/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+
+import { useState } from "react"
+
+export default function Gri(){
+
+    const [file,setFile] = useState<File>()
+
+    return (
+        <>
+        jai mata di
+
+        <div>
+            check here the intial state of the state object
+        </div>
+        </>
+    )
+}
